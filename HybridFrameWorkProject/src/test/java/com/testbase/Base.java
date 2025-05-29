b03bffcf232972de3593814589ef66281f017aec
Admin <Admin@LAPTOP-0NCL7TVA>
2025-05-29 13:56:56 +0530

    updated testbase

--- HybridFrameWorkProject/src/test/java/com/testbase/Base.java
+++ HybridFrameWorkProject/src/test/java/com/testbase/Base.java
@@ -1,5 +1,24 @@
 package com.testbase;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class Base {
+	public WebDriver driver;
+	@BeforeClass
+	public void setup() {
+		driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 
 }
