928bd8c68a32d473f9d02761c9af448c9e72556a
Namrata Pawar <Namrata Pawar@LAPTOP-9F12804G>
2025-05-29 14:27:01 +0530

    my login page java file

--- HybridFrameWorkProject/src/test/java/com/pages/LoginPage.java
+++ HybridFrameWorkProject/src/test/java/com/pages/LoginPage.java
@@ -1,5 +1,59 @@
 package com.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class LoginPage 
+{
+	WebDriver driver;
+	
+	@FindBy(name = "username")
+	WebElement username;
+	
+	@FindBy(name="password")
+	WebElement password;
+	
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement submitBtn;
+	
+	@FindBy(xpath = "//h5[text()='Login']")
+	WebElement loginText;
+	
+	@FindBy(xpath = "//img[@alt='company-branding']")
+	WebElement logoImage;
+	
+ public LoginPage(WebDriver driver)
+ {
+	 this.driver=driver;
+	 PageFactory.initElements(driver, this);
+ }
+
+ public void userNameTextBox() 
+ {
+	 username.sendKeys("Admin");
+ }
+public void passwordTextBox() 
+{
+	password.sendKeys("admin123");
+}
+public void sumbitBtnClick() 
+{
+	submitBtn.click();
+}
+public void loginTextCheck() 
+{
+	if(loginText.isDisplayed())
+	{
+		System.out.println("The text is display correctly");
+	}
+}
+public void logoImgageDisplay() 
+{
+	if(logoImage.isDisplayed()) 
+	{
+		System.out.println("The image is correctly display");
+	}
+}
 }
