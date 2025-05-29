9fc1f14099def552503c04828976e19b8dee7571
Admin <Admin@LAPTOP-0NCL7TVA>
2025-05-29 15:30:59 +0530

    remove parameter

--- HybridFrameWorkProject/src/test/java/com/utility/DropDown.java
+++ HybridFrameWorkProject/src/test/java/com/utility/DropDown.java
@@ -1,33 +1,32 @@
 package com.utility;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class DropDown {
 
-	public static void SelectOptionByVisibleText(WebDriver driver, WebElement ele, String text) {
+	public static void SelectOptionByVisibleText(WebElement ele, String text) {
 		Select options= new Select(ele);
 		options.selectByVisibleText(text);
 	}
-	public static void SelectOptionByValue(WebDriver driver, WebElement ele, String text) {
+	public static void SelectOptionByValue(WebElement ele, String text) {
 		Select options= new Select(ele);
 		options.selectByValue(text);
 	}
-	public static void SelectOptionByIndex(WebDriver driver, WebElement ele, int value) {
+	public static void SelectOptionByIndex(WebElement ele, int value) {
 		Select options= new Select(ele);
 		options.selectByIndex(value);
 	}
 	
-	public static void DeselectOptionByVisibleText(WebDriver driver, WebElement ele, String text) {
+	public static void DeselectOptionByVisibleText(WebElement ele, String text) {
 		Select options= new Select(ele);
 		options.selectByVisibleText(text);
 	}
-	public static void DeselectOptionByValue(WebDriver driver, WebElement ele, String text) {
+	public static void DeselectOptionByValue(WebElement ele, String text) {
 		Select options= new Select(ele);
 		options.selectByValue(text);
 	}
-	public static void DeselectOptionByIndex(WebDriver driver, WebElement ele, int value) {
+	public static void DeselectOptionByIndex(WebElement ele, int value) {
 		Select options= new Select(ele);
 		options.selectByIndex(value);
 	}
